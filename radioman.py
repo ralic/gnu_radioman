2c5dfeef57a3f8de6fed55302582f0b8e1ba05de
Markus Zeindl <mrszndl@googlemail.com>
2009-08-18 01:37:02 +0200

    Better extraction of needed station data, hosting git repo using savannah.nongnu.org

--- radioman.py
+++ radioman.py
@@ -21,42 +21,59 @@
 
 # TODO:
 #  - opportunity for adding web radio
-#  - viewing available stations
+#  d viewing available stations
 #  - deletion of stations
-#  - selection of stations
+#  d selection of stations
 
 import os
 import sys
 
+version = 0.01
+
+logo = """radioman version %s
+Copyright (C) 2009 Markus Zeindl <mrszndl@googlemail.com>
+This program comes with ABSOLUTELY NO WARRANTY; for details see 
+file "LICENSE", stored in the root directory of radioman.
+This is free software, and you are welcome to redistribute it
+under certain conditions; see file "LICENSE", stored in the root directory 
+of radioman.
+""" % (version)
+
+
+# directory where playlist files of radio streams are stored
 stationsdir = "stations"
-ogg_play = "/usr/bin/ogg123"
-mpg_play = "/usr/bin/mpg123"
+
+# available players for different stream types
+# first element of the list is a common extension,
+# the second element is the name of the used codec
+# the third element is the complete path to the player binary
+# Note: radioman executes the binaries in following form:
+# [binary] [stream]
+# For instance: "/usr/bin/ogg123 'http://foo.bar.com/foo.ogg'"
+ogg_play = [ "ogg", "OGG Vorbis", "/usr/bin/ogg123" ]
+mpg_play = [ "mp3", "MPEG", "/usr/bin/mpg123" ]
 
 players = [ ogg_play, mpg_play ]
 
 def play(url):
     # at first, guess type of stream depending of extension
-    # if there is no extension, try mpg_play and ogg_play both
-    if url.endswith("ogg"):
-        os.system("clear")
-        cmd = "%s '%s'" % (ogg_play, url)
-        print "Type of stream is probably ogg vorbis..."
-        print "Trying '%s' " % (cmd)
-        ret = os.system(cmd)
-    elif url.endswith("mp3"):
-        os.system("clear")
-        cmd = "%s '%s'" % (mpg_play, url)
-        print "Type of stream is probably ogg vorbis..."
-        print "Trying '%s' " % (cmd)
-        ret = os.system(cmd)
-    else:
-        for player in players:
+    for player in players:
+        if url.endswith(player[0]):
             os.system("clear")
-            cmd = "%s '%s'" % (player, url)
+            cmd = "%s '%s'" % (player[2], url)
+            print "Type of stream is probably %s..." % (player[1])
             print "Trying '%s' " % (cmd)
             ret = os.system(cmd)
-            if ret == 0:
-                break;
+            return
+    # if type of stream cannot be guessed by extension,
+    # try all above configured player binaries
+    for player in players:
+        os.system("clear")
+        cmd = "%s '%s'" % (player[2], url)
+        print "Trying '%s' " % (cmd)
+        ret = os.system(cmd)
+        if ret == 0:
+            return
 
 def listen(content):
 #    print "listen(%s)" % (content)
@@ -70,23 +87,19 @@ def listen(content):
 
 def viewStations():
     stations = os.listdir(stationsdir)
-    stat_menuitems = dict()
+    stat_menuitems = list()
 
     for z in range(1, len(stations) + 1):
-        station = getStationData(os.getcwd()+
+        stationdata = getStationData(os.getcwd()+
                                  os.sep+
                                  stationsdir+
                                  os.sep+stations[z-1])
-        for addrind in range(1, len(station["url"])+1):
-            if len(station["name"]) == 0:
-                stat_menuitems[str(z)] = [ station["url"][addrind-1], 
-                                           listen, 
-                                           station["url"] ]
-            else:
-                stat_menuitems[str(z)] = [ station["name"][addrind-1],
-                                           listen,
-                                           station["url"]]
-                         
+        for streamdata in stationdata:
+            stat_menuitems.append([ streamdata[0], 
+                                    listen, 
+                                    streamdata[1] ] )
+    stat_menuitems.sort()
+    stat_menuitems.append(["Back to Mainmenu", nulfunc ])
     menu(stat_menuitems, "Stations")
 
 def addStation():
@@ -98,13 +111,16 @@ def editStation():
 def removeStation(): 
     print "remove"
 
+def nulfunc():
+    pass
 
 def menu(items, name=""):
     print "-===========- %s -==============-" % (name)
-    for key in items.keys():
-        print "%s. %s" % (key, items[key][0])
+    for itemind in range(0, len(items)):
+        print "%i. %s" % (itemind+1, items[itemind][0])
     print "Your choice? "
     wahl = raw_input()
+    wahl = int(wahl) - 1
     try:
         if len(items[wahl]) == 2:
             items[wahl][1]()
@@ -115,27 +131,33 @@ def menu(items, name=""):
 
 
 def getStationData(infile):
-    ret = dict()
-    ret["file"] = infile
-    ret["url"] = list()
-    ret["name"] = list()
+    ret = list()
+    name = ""
     streamfile = file(infile)
     content = streamfile.read()
     cont_lines = content.split("\n")
     for line in cont_lines:
         line = line.strip()
         if line.startswith("#EXTINF"):
-            ret["name"].append(line.split(",")[1])
+             name = line.split(",")[1].strip()
         elif line.startswith("http"):
-            ret["url"].append(line)
+            if len(name) == 0:
+                name = line
+            ret.append([name, line, infile])
+            name = ""
     return ret
 
-items = { '1' : [ "View stations", viewStations ],
-          '2' : [ "Add station", addStation],
-          '3' : [ "Edit station", editStation],
-          '4' : [ "Remove station", removeStation ],
-          '0' : [ "Exit", sys.exit, 0]}
 
 
-
-menu(items, "Mainmenu")
+# ---------------------
+#| Program entry point |
+# ---------------------
+if __name__ == "__main__":
+    print logo
+    while(True):
+        items = [ [ "View stations", viewStations ],
+                  [ "Add station", addStation],
+                  [ "Edit station", editStation],
+                  [ "Remove station", removeStation ],
+                  [ "Exit", sys.exit, 0] ]
+        menu(items, "Mainmenu")
